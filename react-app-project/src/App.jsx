d18ed298dbbc5a86c93e8d237cb39e0562ff36df
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-15 14:54:42 -0500

    box styling

--- react-app-project/src/App.jsx
+++ react-app-project/src/App.jsx
@@ -5,7 +5,14 @@ import './App.css'
 
 const Card = ({ title }) => {
   return (
-    <div> 
+    <div className="card" style={{
+      border: '1px solid #4b5362',
+      padding: '20px',
+      margin: "10px",
+      backgroundColor: '#31363f', 
+      borderRadius: '10px',
+      minHeight: '100px',
+    }}> 
       <h2> {title}</h2>
     </div>
   )
@@ -13,8 +20,7 @@ const Card = ({ title }) => {
 
 const App = () => {
   return (
-    <div>
-    <h2>Functional Arrow Component</h2>
+    <div className="card-container">
     <Card title="Star Wars" />
     <Card title="Avatar"/>
     <Card title="The Lion King" />
