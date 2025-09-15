7158587e0b3fc108689e59b67f096d155663d3e7
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-15 14:40:34 -0500

    titles

--- react-app-project/src/App.jsx
+++ react-app-project/src/App.jsx
@@ -3,10 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const Card = () => {
+const Card = ({ title }) => {
   return (
     <div> 
-      <h2> Card Component</h2>
+      <h2> {title}</h2>
     </div>
   )
 }
@@ -15,7 +15,9 @@ const App = () => {
   return (
     <div>
     <h2>Functional Arrow Component</h2>
-    <Card />
+    <Card title="Star Wars" />
+    <Card title="Avatar"/>
+    <Card title="The Lion King" />
     </div>
   )
 }
