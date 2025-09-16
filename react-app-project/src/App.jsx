ce15820b82e66384106342b16c2797f3d4253fd0
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-16 12:40:05 -0500

    update boxes

--- react-app-project/src/App.jsx
+++ react-app-project/src/App.jsx
@@ -5,20 +5,15 @@ import './App.css'
 
 const Card = ({ title }) => {
   return (
-    <div className="card" style={{
-      border: '1px solid #4b5362',
-      padding: '20px',
-      margin: "10px",
-      backgroundColor: '#31363f', 
-      borderRadius: '10px',
-      minHeight: '100px',
-    }}> 
+    <div className="card"> 
       <h2> {title}</h2>
     </div>
   )
 }
 
 const App = () => {
+  const [hasLiked, setHasLiked] = useState();
+
   return (
     <div className="card-container">
     <Card title="Star Wars" />
