403a96d22f341dfb9f172318bb65503dc28863cd
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-16 15:56:56 -0500

    app update

--- react-app-project/src/App.jsx
+++ react-app-project/src/App.jsx
@@ -4,19 +4,23 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 const Card = ({ title }) => {
+const [hasLiked, setHasLiked] = useState(false);
   return (
     <div className="card">
       <h2> {title}</h2>
+
+      <button onClick={() => setHasLiked(true)}>
+        Like
+      </button>
     </div>
   );
 };
 
 const App = () => {
-  const [hasLiked, setHasLiked] = useState();
 
   return (
     <div className="card-container">
-      <Card title="Star Wars" />
+      <Card title="Star Wars" rating={5} isCool={true} />
       <Card title="Avatar" />
       <Card title="The Lion King" />
     </div>
