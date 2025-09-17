c18e64f2fcaf1e216db1d2f2218f849e44657857
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-17 15:42:23 -0500

    background

--- react-app-project/src/App.jsx
+++ react-app-project/src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
       <div className="pattern" />
       <div className="wrapper">
         <header>
+          <img src="./hero.png" alt="Hero Banner"/>
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassel</h1>
         </header>
 
