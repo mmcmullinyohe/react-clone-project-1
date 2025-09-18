d42ac2ab760e27499896673358b9cfd506592db8
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-18 15:47:03 -0500

    spelling fix

--- react-app-project/src/components/search.jsx
+++ react-app-project/src/components/search.jsx
@@ -7,7 +7,7 @@ const Search = ({ searchTerm, setSearchTerm }) => {
               <img src="search.svg" alt="search" /> 
               <input 
               type="text"
-              placeholder="Search thougth thousands of movies"
+              placeholder="Search through thousands of movies"
               value={searchTerm}
               onChange={(event) => setSearchTerm(event.target.value)}
               /> 
