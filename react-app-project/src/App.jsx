b66ae03a00d660d21f07f0674e3ca3f2e19c6a72
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-18 13:11:12 -0500

    searchTerm

--- react-app-project/src/App.jsx
+++ react-app-project/src/App.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./components/search";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <main>
       <div className="pattern" />
@@ -11,7 +13,7 @@ const App = () => {
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassel</h1>
         </header>
 
-        <Search />
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
     </main>
   )
