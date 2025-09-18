4c3bf349f73eb1848cd308b9485605023689fbad
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-18 16:18:21 -0500

    undo change

--- react-app-project/src/App.jsx
+++ react-app-project/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
           <img src="./hero.png" alt="Hero Banner"/>
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassel</h1>
         </header>
-testing
+
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         <h1 className="text-white">{searchTerm}</h1>
       </div>
