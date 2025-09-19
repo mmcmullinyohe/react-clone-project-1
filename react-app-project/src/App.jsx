74dcd4ffe1da1a7da05d6012cdddd11f9a6c418d
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-19 14:19:07 -0500

    api updates

--- react-app-project/src/App.jsx
+++ react-app-project/src/App.jsx
@@ -3,6 +3,16 @@ import Search from "./components/search";
 
 const API_BASE_URL= 'https://api.themoviedb.org/3'
 
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+ method:"GET", 
+ headers: {
+  accept: 'application/json',
+  Authorization: `Bearer ${API_KEY}`
+ }
+}
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
 
